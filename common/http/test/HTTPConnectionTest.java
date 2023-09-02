18c9a28a2bfcf4bbfabd6cbfeedf9429f84cd769
Lennox Avdiu <a1774765@adelaide.edu.au>
2023-09-02 09:38:41 +0930

    played around with necessary dependencies for mocking class objects and determining if the necessary method was actually called without running the functionality. Now we can fully unit test all of the components separaetely without requiring genuine network connections. Important note: I think I should leave the integration tests for later as my tests will most likely change quite frequently as the functionality of the components grow/change.

--- common/http/test/HTTPConnectionTest.java
+++ common/http/test/HTTPConnectionTest.java
@@ -8,11 +8,18 @@ import org.mockito.Mockito;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.Socket;
 
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.whenNew;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(HTTPConnection.class)
 public class HTTPConnectionTest {
     private static final String mockRequest = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
     private static final String mockResponse = "HTTP/1.1 200 OK\r\n\r\nMock server response!";
@@ -28,14 +35,48 @@ public class HTTPConnectionTest {
     }
 
     @Test
-    public void testSendRequest() throws IOException {
-        String response;
+    public void testSendRequest() throws Exception {
+        OutputStreamWriter mockOut = Mockito.mock(OutputStreamWriter.class);
+        when(mockServerSocket.getOutputStream()).thenReturn(new ByteArrayOutputStream());
+        // Whenever a new OutputStreamWriter is created, return the mockOut
+        whenNew(OutputStreamWriter.class).withAnyArguments().thenReturn(mockOut);
+
         try (HTTPConnection conn = new HTTPConnection(mockServerSocket)) {
-            // Make the request
             conn.sendRequest(mockRequest);
-            response = conn.readResponse();
         }
         
-        assertEquals(response, mockResponse);
+        // Verify that the mockOut stub was written to
+        verify(mockOut).write(mockRequest, 0, mockRequest.length());
+    }
+
+    @Test
+    public void testReadResponse() throws IOException {
+        when(mockServerSocket.getInputStream()).thenReturn(new ByteArrayInputStream(mockResponse.getBytes()));
+
+        String response;
+        try (HTTPConnection conn = new HTTPConnection(mockServerSocket)) {
+            response = conn.readResponse();
+        }
+
+        assertEquals(mockResponse, response);
+    }
+
+    @Test
+    public void testReadResponseEmptyResponse() throws IOException {
+        String emptyResponse = "";
+        when(mockServerSocket.getInputStream()).thenReturn(new ByteArrayInputStream(emptyResponse.getBytes()));
+
+        try (HTTPConnection conn = new HTTPConnection(mockServerSocket)) {
+            String response = conn.readResponse();
+            assertEquals(emptyResponse, response);
+        }
+    }
+
+    @Test
+    public void testClose() throws IOException {
+        try (HTTPConnection conn = new HTTPConnection(mockServerSocket)) {
+            // Just open and close
+        }
+        verify(mockServerSocket, times(1)).close();
     }
 }
