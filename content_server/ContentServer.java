275ebf635b90afb8703379f499d885058560d98e
Lennox Avdiu <a1774765@adelaide.edu.au>
2023-09-02 19:59:44 +0930

    added test for ContentServer to ensure that its version of buildRequest works as intended

--- content_server/ContentServer.java
+++ content_server/ContentServer.java
@@ -1,13 +1,18 @@
 package content_server;
 
 import java.io.IOException;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import client.GETClient;
 import common.http.HTTPClient;
 
 
-public class ContentServer extends HTTPClient {    
+public class ContentServer extends HTTPClient {
+    private static final String HOST = "127.0.0.1";
+    private static final int PORT = 4567;
+    
     private final String request_location = "content_server/resources/PUTRequest.txt";
     private final String payload_file = "content_server/resources/WeatherData.txt";
 
@@ -20,6 +25,10 @@ public class ContentServer extends HTTPClient {
     }
 
     public static void main(String[] args) {
-        new ContentServer().run();
+        try {
+            new ContentServer().run(new Socket(HOST, PORT));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ content_server/test/content_server/ContentServerTest.java
@@ -0,0 +1,50 @@
+package content_server;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({Files.class})
+public class ContentServerTest {
+
+    private static final String requestLocation = "content_server/resources/PUTRequest.txt";
+    private static final String payloadFileTest = "content_server/resources/TestWeatherData.txt";
+    private static final String expectedRequestTemplate = "PUT /weather.json HTTP/1.1\n" + //
+                                                          "User-Agent: ATOMClient/1/0\n" + //
+                                                          "\n" + //
+                                                          "{{payload}}";
+    private static final String expectedPayload = "Some sample weather data here.";
+    private static final String expectedRequest = expectedRequestTemplate.replace("{{payload}}", expectedPayload);
+
+    @InjectMocks
+    ContentServer contentServer;
+
+    @Before
+    public void setUp() throws IOException {
+        MockitoAnnotations.initMocks(this);
+        
+        mockStatic(Files.class);
+        when(Files.readAllBytes(Paths.get(requestLocation))).thenReturn(expectedRequestTemplate.getBytes());
+        when(Files.readAllBytes(Paths.get(payloadFileTest))).thenReturn(expectedPayload.getBytes());
+    }
+
+    @Test
+    public void testBuildRequest() throws IOException {
+        String result = contentServer.buildRequest(requestLocation, payloadFileTest);
+        assertEquals(expectedRequest, result);
+    }
+}
